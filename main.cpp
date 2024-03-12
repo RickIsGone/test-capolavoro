f866ba51dd19539342b6bf6d21ad83e6939c070f
RickIsGone <riccardotedeschi247@gmail.com>
2024-03-12 17:19:23 +0100

    creazione progetto capolavoro

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#define SDL_MAIN_HANDLED
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h> 
+#include <SDL_main.h>
+#include <SDL_mixer.h>
+
+
+int main(int argc,char* argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+
+    
+    
+    SDL_Quit();
+    TTF_Quit();
+    Mix_Quit();
+    return EXIT_SUCCESS;
+}
